4898c2662f6a2e3aaf3881263c1d315f867ae4e9
Shivam Maheshwari <shivamannu7@gmail.com>
2023-11-23 12:05:57 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const Title = ()=>(
+    <h1>Namaste React Title Component</h1>
+);
+
+const Heading = ()=>(
+    <div id="container">
+        <Title/>
+        <h1>Namaste React Heading Component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<Heading/>);
